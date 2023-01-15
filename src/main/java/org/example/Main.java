9ce6258b2b506a1697f60ffd9a98b706ddfc42dc
unknown <ahmadmaen15@gmail.com>
2023-01-15 12:36:20 +0300

    added by user2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,7 @@ public class Main {
     public static void main(String[] args) {
         //Suiiiiii
         System.out.println("Hello world!");
+        int a=0,b=4;
+        System.out.println(a+b);
     }
 }
